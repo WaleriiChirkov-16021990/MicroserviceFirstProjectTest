9fe2fe897f266771ce7314b98a776b55a8df1c05
WaleriiChirkov-16021990 <zx84058405@live.com>
2024-02-22 00:06:41 +0300

    starter TimerAspect 2

--- cus-timer-starter/src/main/java/ru/chirkov/aspect/ExecutionForReturnObject.java
+++ cus-timer-starter/src/main/java/ru/chirkov/aspect/ExecutionForReturnObject.java
@@ -2,11 +2,11 @@ package ru.chirkov.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 @Aspect
 @Slf4j
@@ -15,7 +15,7 @@ public class ExecutionForReturnObject {
     public void executeAction() {
     }
 
-    @Around("executeAction()")
+    @Before("executeAction()")
     public void endProceed(JoinPoint point) {
         log.info("Method : [" + point.getSignature().getName() + "], for return type : " + point.getSignature().getDeclaringType().getName() + "started (" + System.currentTimeMillis() + ")");
     }
